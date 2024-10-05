b4f030a44bdf177a49f1914734f85c8140da79e3
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-10-05 22:04:46 +0530

    font corr

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     <div className="bg-black w-full h-screen text-white flex justify-center items-center">
       <button 
         onClick={handleClick}
-        className="text-[30px] font-bold py-[36px] px-[44px] border-2 border-dashed border-white/[.4] hover:border-white ease-in-out transition"
+        className="text-[30px] font-semibold py-[36px] px-[44px] border-2 border-dashed border-white/[.4] hover:border-white ease-in-out transition"
       >
         Get Started
       </button>
