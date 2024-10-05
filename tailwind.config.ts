0938248697c8310519ac06f3104516fbcb9491a1
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2024-10-05 20:14:10 +0530

    Add configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: "#000",
+        white: "#FFF",
       },
     },
   },
