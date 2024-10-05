4afced3be68c1bb4eaccfb5fbd7d90ad6b7eee18
Elvin2605 <145311033+Elvin2605@users.noreply.github.com>
2024-10-05 22:38:53 +0530

    done the intro

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='bg-black h-screen w-screen'>
+      <p className='flex justify-center text-white text-center items-center text-[34.2px] pt-[250px] font-semibold'>So, what’re you tryna create?</p>
+      <p className='flex justify-center text-white text-center items-center text-[16.2px]  font-semibold'>Just pick what you need!</p>
+    </div>
   )
 }
 
